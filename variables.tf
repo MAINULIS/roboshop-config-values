e1e12ffbb0a1f1b9074a76dd78bb69f62492c758
Mainul Islam <mainulislam6168@gmail.com>
2025-07-20 08:55:11 +0600

    troubleshooting

--- variables.tf
+++ variables.tf
@@ -18,5 +18,18 @@ variable "secrets" {
         CATALOGUE_PORT       = 8080
       }
     }
+
+    frontend = {
+      secret_mount = "roboshop-dev"
+      kv = {
+        CATALOGUE_URL           = "http://catalogue-dev.maidevops.fun:8200/",
+        USER_URL           = "http://user-dev.maidevops.fun:8200/",
+        CART_URL           = "http://cart-dev.maidevops.fun:8200/",
+        SHIPPING_URL           = "http://shipping-dev.maidevops.fun:8200/",
+        PAYMENT_URL           = "http://payment-dev.maidevops.fun:8200/",
+        CATALOGUE_HOST       = "catalogue-dev.maidevops.fun",
+        CATALOGUE_PORT       = 8080
+      }
+    }
   }
 }
