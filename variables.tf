a25d72e5395550daceb20e949939700638538287
Mainul Islam <mainulislam6168@gmail.com>
2025-07-20 10:16:51 +0600

    added shipping config info

--- variables.tf
+++ variables.tf
@@ -13,20 +13,20 @@ variable "secrets" {
     cart = {
       secret_mount = "roboshop-dev"
       kv = {
-        REDIS_HOST           = "redis-dev.maidevops.fun",
-        CATALOGUE_HOST       = "catalogue-dev.maidevops.fun",
-        CATALOGUE_PORT       = 8080
+        REDIS_HOST          = "redis-dev.maidevops.fun",
+        CATALOGUE_HOST      = "catalogue-dev.maidevops.fun",
+        CATALOGUE_PORT      = 8080
       }
     }
 
     frontend = {
       secret_mount = "roboshop-dev"
       kv = {
-        CATALOGUE_URL      = "http://catalogue-dev.maidevops.fun:8200/",
-        USER_URL           = "http://user-dev.maidevops.fun:8200/",
-        CART_URL           = "http://cart-dev.maidevops.fun:8200/",
-        SHIPPING_URL       = "http://shipping-dev.maidevops.fun:8200/",
-        PAYMENT_URL        = "http://payment-dev.maidevops.fun:8200/"
+        CATALOGUE_URL       = "http://catalogue-dev.maidevops.fun:8200/",
+        USER_URL            = "http://user-dev.maidevops.fun:8200/",
+        CART_URL            = "http://cart-dev.maidevops.fun:8200/",
+        SHIPPING_URL        = "http://shipping-dev.maidevops.fun:8200/",
+        PAYMENT_URL         = "http://payment-dev.maidevops.fun:8200/"
       }
     }
 
@@ -50,23 +50,25 @@ variable "secrets" {
     mysql = {
       secret_mount = "roboshop-dev"
       kv = {
-        ROOT_PASSWORD        = "Roboshop@1"
+        ROOT_PASSWORD       = "Roboshop@1"
       }
     }
 
     rabbitmq = {
       secret_mount = "roboshop-dev"
       kv = {
-        APP_USER              = "roboshop",
-        APP_PASSWORD          = "roboshop123"
+        APP_USER             = "roboshop",
+        APP_PASSWORD         = "roboshop123"
       }
     }
 
     shipping = {
       secret_mount = "roboshop-dev"
       kv = {
-        CART_ENDPOINT           = "cart-dev.maidevops.fun:8080",
-        DB_HOST                 = "mysql-dev.maidevops.fun"
+        CART_ENDPOINT         = "cart-dev.maidevops.fun:8080",
+        DB_HOST               = "mysql-dev.maidevops.fun",
+        DB_USER               = "root",
+        DB_PASS               = "Roboshop@1"
       }
     }
 
