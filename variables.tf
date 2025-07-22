5452474f90a40d1034f9fc489f6896d8832a1852
Mainul Islam <mainulislam6168@gmail.com>
2025-07-22 22:37:24 +0600

    docker shipping code

--- variables.tf
+++ variables.tf
@@ -86,6 +86,8 @@ variable "secrets" {
         DB_HOST               = "mysql-dev.maidevops.fun"
         DB_USER               = "root"
         DB_PASS               = "Roboshop@1"
+        DB_TYPE               = "mysql"
+        APP_GIT_URL           = "https://github.com/roboshop-devops-project-v3/shipping"
       }
     }
 
